99519729fef2eecbd47bc4c50f4cae5e7511a77e
Niko Tepe <niko.tepe@gmail.com>
2023-12-07 12:39:00 +0100

    Aufgabe 1 done

--- Cross_a.c
+++ Cross_a.c
@@ -1,8 +1,112 @@
-#include <stdio.h> 
-#include <pthread.h> 
-#include <semaphore.h> 
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
 
-int main(){
-    printf("Hello World!");
+// https://www.youtube.com/watch?v=ukM_zzrIeXs
+
+const char *directions[] = {"/north", "/east", "/south", "/west"};
+
+enum directions
+{
+    north, // 0
+    east,  // 1
+    south, // 2
+    west   // 3
+};
+
+enum directions string_to_enum(const char *str)
+{
+    if (strcmp(str, "north") == 0)
+        return north;
+    if (strcmp(str, "east") == 0)
+        return east;
+    if (strcmp(str, "south") == 0)
+        return south;
+    if (strcmp(str, "west") == 0)
+        return west;
+
+    fprintf(stderr, "Ungültige Richtung: %s\n", str);
+    exit(1);
+}
+
+void signalHandler()
+{
+    printf("SIGINT signal received. Exiting...\n");
+    // Perform any cleanup operations here
+    // sem_post(sem_src);
+    // sem_post(sem_dest);
+
+    // sem_close(sem_src);
+    // sem_close(sem_dest);
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+
+    signal(SIGINT, signalHandler);
+
+    pid_t pid = getpid();
+    srand(pid);
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "Bitte genau zwei Argumente angeben\n");
+        return 1;
+    }
+
+    enum directions car_src = string_to_enum(argv[1]);
+    enum directions car_dest = string_to_enum(argv[2]);
+
+    if (car_src == car_dest)
+    {
+        fprintf(stderr, "Quelle und Ziel dürfen nicht gleich sein\n");
+        return 1;
+    }
+
+    sem_t *sem_src = sem_open(directions[car_src], O_CREAT, 0666, 1);
+    if (sem_src == SEM_FAILED)
+    {
+        perror("sem_open/sem_src");
+        exit(EXIT_FAILURE);
+    }
+
+    sem_t *sem_dest = sem_open(directions[car_dest], O_CREAT, 0666, 1);
+    if (sem_dest == SEM_FAILED)
+    {
+        perror("sem_open/sem_dest");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Kaefer %i: ich komme von %s.\n", pid, argv[1]);
+
+    sem_wait(sem_src);
+
+    printf("Kaefer %i: ich stehe nun bei %s.\n", pid, argv[1]);
+    // usleep(1000000 - 100 * (rand() % 10));
+    sleep(3);
+    printf("Kaefer %i: ist %s frei?\n", pid, argv[2]);
+
+    sem_wait(sem_dest);
+
+    printf("Kaefer %i: Es ist frei!\n", pid);
+    printf("Kaefer %i: Ich fahre los nach %s.\n", pid, argv[2]);
+
+    sleep(1);
+
+    sem_post(sem_src);
+
+    printf("Kaefer %i: ich bin angekommen in %s.\n", pid, argv[2]);
+
+    sem_post(sem_dest);
+
+    sem_close(sem_src);
+    sem_close(sem_dest);
     return 0;
 }
